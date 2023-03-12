a742e275ed527c441c3f11e8f71fe29b357b533b
dwayneInterino <interinoandrewdwayne@gmail.com>
2023-03-12 12:45:19 +0800

    small changes

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ from whileLoop import loopWhile
 import time
 input = int(input("Input a number: "))
 
-time_start = time.time()
+while_time_start = time.time()
 loopWhile(input)
-time_end = time.time()
-time_total = time_end - time_start
-print("While loop time: {0}" .format(time_total))
+while_time_end = time.time()
+while_time_total = while_time_end - while_time_start
+print("While loop time: {0}" .format(while_time_total))
 
-time_start = time.time()
+for_time_start = time.time()
 loopFor(input)
-time_end = time.time()
-time_total = time_end - time_start
-print("For loop time: {0}" .format(time_total))
+for_time_end = time.time()
+for_time_total = for_time_end - for_time_start
+print("For loop time: {0}" .format(for_time_total))
